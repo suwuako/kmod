cf8b40be88579937e6e0ecf5f581b2d64059ba19
suwa <ronnielow@protonmail.com>
2026-09-07 21:12:00 +1000

    stupid newline bug

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -7,12 +7,12 @@ MODULE_AUTHOR("meow");
 MODULE_DESCRIPTION("hello world meow");
 
 static int __init hello_init(void) {
-    printk("meow: hi my name is module");
+    printk("meow: hi my name is module\n");
     return 0;
 }
 
 static void __exit hello_exit(void) {
-    printk("meow: bye my name is module and im done haha");
+    printk("meow: bye my name is module and im done haha\n");
 }
 
 module_init(hello_init);
